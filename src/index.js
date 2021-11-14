d71d231ac07cc65cc21dac0f6446f28ba453e22c
himanshu Tiwari <72072039+techhimanshu-beep@users.noreply.github.com>
2021-11-14 17:59:18 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+let curDate = new Date();
+curDate = curDate.getHours();
+let gretting = "";
+const cssStyle = {};
+
+if (curDate >= 1 && curDate < 12) {
+  gretting = "Good Morning";
+  cssStyle.color = "green";
+} else if (curDate >= 12 && curDate < 19) {
+  gretting = "Good Afternoon";
+  cssStyle.color = "orange";
+} else {
+  gretting = "Good Night";
+  cssStyle.color = "black";
+}
+
+
+ReactDOM.render(
+  <>
+    <div>
+      <h1> hey himanshu, <span style={cssStyle}> {gretting} </span>  </h1>
+    </div>
+  </>,
+  document.getElementById('root')
+);
+
+
